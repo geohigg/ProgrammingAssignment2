5fb81a8bb8104b03db108cc6963d7b1377bc2e35
GeoHIGG <JPHigginsGIS@GMAIL.com>
2015-02-22 12:39:50 -0500

    updated the message in the cacheSolve function

--- cachematrix.R
+++ cachematrix.R
@@ -31,7 +31,7 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inverse.x <-x$getinverse()
         if(!is.null(inverse.x)){
-                message("getting the cached reversed matrix")
+                message("getting the cached inversed matrix")
                 return(inverse.x)
         }
         
